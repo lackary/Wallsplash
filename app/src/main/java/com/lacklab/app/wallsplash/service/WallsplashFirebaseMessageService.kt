1da22731e77cfbefb670fa3b2211cca44c52d149
lackary <lackary@gmail.com>
2021-10-18 15:43:30 +0800

    Add FirebaseMessage Service

---
+++ app/src/main/java/com/lacklab/app/wallsplash/service/WallsplashFirebaseMessageService.kt
@@ -0,0 +1,71 @@
+package com.lacklab.app.wallsplash.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.lacklab.app.wallsplash.MainActivity
+import com.lacklab.app.wallsplash.R
+import timber.log.Timber
+
+class WallsplashFirebaseMessageService : FirebaseMessagingService() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        Timber.d("From: ${p0.from}")
+        if (p0.data.isNotEmpty()) {
+            Timber.d("Message data payload: ${p0.data}")
+        }
+
+        // Check if message contains a notification payload.
+        p0.notification?.let {
+            Timber.d("Message Notification Body: ${it.body}")
+            sendNotification(it)
+        }
+    }
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Timber.d("Refreshed token: $p0")
+    }
+
+    private fun sendNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+//            .setContentTitle(getString(R.string.fcm_message))
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
