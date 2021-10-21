49b528a1d57bc4513bc0122c27bce1bf3acee1bd
lackary <lackary@gmail.com>
2021-10-21 14:48:00 +0800

    navigate to search fragment from bottom appbar

--- app/src/main/java/com/lacklab/app/wallsplash/view/SearchActivity.kt
+++ app/src/main/java/com/lacklab/app/wallsplash/view/SearchActivity.kt
@@ -1,6 +1,18 @@
 package com.lacklab.app.wallsplash.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lacklab.app.wallsplash.databinding.ActivitySearchBinding
+import timber.log.Timber
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val funName = object{}.javaClass.enclosingMethod.name
+        Timber.d("")
+
+    }
 }
